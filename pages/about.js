2e6f157d9b4258d2e4fe508a96b2f4dbef78bba8
장성민 <earnestgem@optatumplatform.com>
2022-05-11 14:46:33 +0900

    css modules

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import NavBar from "../components/navbar";
 
-export default function Home() {
+export default function About() {
   return (
     <div>
       <NavBar/>
-      <h1>Hello world</h1>
+      <h1>This is about page!</h1>
     </div>
-  );
+  )
 }
